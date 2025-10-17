324c29a0a1baecc71a6bc0176aeb80a8ddb747e9
marcoscassian <marcozcassiano@gmail.com>
2025-10-17 17:44:41 -0300

    teste de envio de arquivos

---
+++ flask_upload_pdf.py
@@ -0,0 +1,48 @@
+#responsavel por fazer os uploads dos pdfs dos arquivos ao banco de dados
+
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from flask import send_from_directory, jsonify
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "static/img/teste"
+ALLOWED_EXTENSIONS = {'pdf', 'png'}
+TAMANHO_PERMITIDO = 16 * 1000 * 1000
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = TAMANHO_PERMITIDO
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        
+        # verifique se a solicitacao de postagem tem a parte do arquivo
+        if 'file' not in request.files:
+            flash('Nao tem a parte do arquivo')
+            return redirect(request.url)
+        file = request.files['file']
+        
+        # Se o usuario nao selecionar um arquivo, o navegador envia um
+        # arquivo vazio sem um nome de arquivo.
+        
+        if file.filename == '':
+            flash('Nenhum arquivo selecionado')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return render_template('flask_upload_pdf.html')
+    
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+if __name__ == '__main__':
+    app.run(debug = True)
